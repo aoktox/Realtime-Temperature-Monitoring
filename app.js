4a518309d87fd4da4c68b440ee4fee39bf67b072
Agus Prasetiyo <agus@prasetiyo.id>
2017-01-30 20:13:58 +0700

    Initial

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var path = require('path');
 var io = require('socket.io')(http);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 3000);
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
 
 app.get('/',function (req, res) {
     res.sendFile(path.join(__dirname+'/index.html'));
@@ -18,7 +20,10 @@ io.on('connection',function (socket) {
     });
 });
 
-http.listen(80);
+http.listen(app.get('port'), app.get('ipaddr'), function(){
+    console.log('Express server listening on  IP: ' + app.get('ipaddr') + ' and port ' + app.get('port'));
+});
+// http.listen(80);
 // http.listen(3000,function () {
 //     console.log('App start on port 3000');
 // });
