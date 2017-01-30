df95006da7b56c8781511bf31cb8ec2871eadf7d
Agus Prasetiyo <agus@prasetiyo.id>
2017-01-30 16:02:48 +0700

    Initial

---
+++ app.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var path = require('path');
+var io = require('socket.io')(http);
+
+app.get('/',function (req, res) {
+    res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+io.on('connection',function (socket) {
+    console.log('Client connected');
+    socket.on('suhu',function (data) {
+        /*console.log(data);*/
+        socket.broadcast.emit('suhuku', data);
+    });
+    socket.on('disconnect',function () {
+        /*console.log('Client dicconnected');*/
+    });
+});
+
+
+http.listen(3000,function () {
+    console.log('App start on port 3000');
+});
